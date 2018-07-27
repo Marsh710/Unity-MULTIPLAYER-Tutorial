8a892b07814f7d9f2189d796d4100c3f12cdeb72
Marsh710 <marshall.hutto@gmail.com>
2018-07-27 09:00:34 -0400

    Additional changes

--- Unity MULTIPLAYER Tutorial/Assets/Scenes/PlayerObject.cs
+++ Unity MULTIPLAYER Tutorial/Assets/Scenes/PlayerObject.cs
@@ -5,10 +5,9 @@ using UnityEngine.Networking;
 
 public class PlayerObject : NetworkBehaviour {
 
-	// Use this for initialization
 	void Start () {
         // Is this actually my own local PlayerObject?
-        if( isLocalPlayer ==false)
+        if( isLocalPlayer == false)
         {
             // This object belongs to another pplayer.
             return;
@@ -16,6 +15,7 @@ public class PlayerObject : NetworkBehaviour {
 
         // Sine the PlayerObjcet  is invisible and  not part of the world,
         // give me something physical to move around!
+
         Debug.Log("PlayerObject::Start  -- Spawning my own personal unit.");
 
         // Instantiate() only creates an object on the LOCAL COMPUTER.
@@ -23,13 +23,29 @@ public class PlayerObject : NetworkBehaviour {
         // the network (and therefore not on any other client) UNLESS
         // NetworkServer.Spawn() is called on this object.
 
-        Instantiate(PlayerUnitPtefab);
+        //Instantiate(PlayerUnitPrefab);
+
+        // Command the server to SPAWN our unit
+
+        CmdSpawnMyUnit();
 	}
 
-    public GameObject PlayerUnitPtefab;
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject PlayerUnitPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+    } 
+    /////////////////////////  COMMANDS
+    // Commands are special functions that only get executed on the server.
+
+    [Command]
+    void CmdSpawnMyUnit()
+    {
+    GameObject go = Instantiate(PlayerUnitPrefab);
+
+    // Now that the object exists on the server, propagate it to all
+    // the clients (and aslo wire up the NetworkIdentity)
+    NetworkServer.Spawn(go);
+    }
 }
